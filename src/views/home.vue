dd80590738b46310ab8b41d9a70edeea36c28508
Sebastian Mariani <sebastian.mariani.94@gmail.com>
2021-09-15 16:56:31 +0100

    modify homepage layout

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,79 +1,102 @@
 <template>
-    <div class="home">
-        <h2 id="top">We are changing <br> the world of art by...</h2>
-        <div class="img-with-text">
-            <img src="../assets/museum.jpg" alt="">
-            <h2 id="main">Making art <br> accessible to <br> everyone.</h2> 
-        </div>
+  <div class="home">
+    <div class="split">
+      <div class="text">
+        <h3>
+          We are changing the world of art <br />
+          by making art accessible to everyone.
+        </h3>
+        <router-link to="/browse"><button>Start now</button></router-link>
+      </div>
+      <img src="../assets/homepageImg.png" alt="" />
     </div>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
 .home {
-    padding:0 5%;
-    font-size: 2em;
-    color:#eb5e55;
+  padding: 0 5%;
+  font-size: 1.5em;
+  color: #eb5e55;
 }
-#top {
-    text-align: center;
-    padding:0 10%;
+.split {
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 3rem;
 }
-.img-with-text{
-    position: relative;
-    margin-bottom:4%;
+.text {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: flex-start;
 }
-.img-with-text h2 {
-    position: absolute;
-    top:7em;
-    left: 65%;
+h3 {
+  margin-top: 0;
 }
-.img-with-text img {
-    width: 60%;
-    height: 100%;
+button {
+  padding: 10px 30px;
+  background-color: #243e36;
+  color: #eb5e55;
+  border-radius: 5px;
+  border: none;
+  font-size: inherit;
 }
-hr {
-        border: 1px solid #eb5e55;
-        margin-bottom: 4%;
-    }
-@media only screen and (max-width: 1982px) {
-     .home {
-        font-size: 1.5em;
-    }
+a {
+  margin: 0 auto;
 }
 @media only screen and (max-width: 1480px) {
-    .home {
-    font-size: 1.3em;
-    }
+  .home {
+    font-size: 1.4em;
+  }
 }
 @media only screen and (max-width: 1250px) {
-    .home {
-    font-size: 1em;
-    }
+  .home {
+    font-size: 1.2em;
+  }
+  img {
+    width: 400px;
+  }
 }
 @media only screen and (max-width: 1050px) {
-    .home {
-    font-size: .7em;
-    }
+  .home {
+    font-size: 0.9em;
+  }
+  img {
+    width: 300px;
+  }
+}
+@media only screen and (max-width: 890px) {
+  .home {
+    font-size: 0.6em;
+  }
+  img {
+    width: 200px;
+  }
+  button {
+    padding: 8px 10px;
+  }
 }
 @media only screen and (max-width: 625px) {
-    .home {
-    font-size: .4em;
-    }
+  .home {
+    font-size: 0.4em;
+  }
+  img {
+    width: 150px;
+  }
+  button {
+    padding: 5px 8px;
+  }
 }
 @media only screen and (max-width: 490px) {
-    .home {
-    font-size: .4em;
-    }
-}
-@media only screen and (max-width: 391px) {
-    .img-with-text h2 {
-        top:2em;
-    }
+  .home {
+    font-size: 0.4em;
+  }
+  img {
+    display: none;
+  }
 }
 </style>
